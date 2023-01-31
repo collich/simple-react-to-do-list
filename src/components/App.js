8160eb13a79a5f0d844d366765177ba0c9901f2f
Collin <collin-chua@hotmail.com>
2023-01-31 18:16:38 +0800

    Refactor code to make it work with local db.json file

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,60 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import ListCreate from './ListCreate'
 import TodoList from './TodoList'
 
 const App = () => {
     const [list, setList] = useState([])
 
+    const getLists = async () => {
+       const response = await axios.get("http://localhost:3001/lists")
 
-    const handleCreate = (title, description) => {
-        const updatedList = [
-            ...list,
-            {id: (list.length === 0 ? 1: list.length + 1) ,title, description}
-        ]
-        setList(updatedList)
+       setList(response.data)
     }
 
-    const handleDelete = (id) => {
+    useEffect(() => {
+        getLists()
+    }, [])
+    
+
+
+    const handleCreate = async (title, description) => {
+        const response = await axios.post("http://localhost:3001/lists", {
+            title,
+            description
+        })
+        
+        const updatedList = [
+            ...list,
+            response.data
+        ]
+        setList(updatedList)
+
+        // const updatedList = [
+        //     ...list,
+        //     {id: (list.length === 0 ? 1: list.length + 1) ,title, description}
+        // ]
+        // setList(updatedList)
+    }
+
+    const handleDelete = async (id) => {
+        await axios.delete(`http://localhost:3001/lists/${id}`)
+
         const deleteList = list.filter(li => {
             return id !== li.id
         })
         setList(deleteList)
     }
 
-    const handleEdit = (id, title, description) => {
+    const handleEdit = async (id, title, description) => {
+        const response = await axios.put(`http://localhost:3001/lists/${id}`, {
+            title,
+            description
+        })
+
         const updatedTodo = list.map(li => {
             if (li.id === id) {
-                return {...li, title, description}
+                return {...li, ...response.data}
             }
             return li
         })
